b7de9632b8fcebe0fdd629ff6a7a7b50774b32be
Shashankvjogi <shashankjogi111@gmai.com>
2022-09-16 15:04:53 +0530

    Added project to git

---
+++ src/pack1/Addition.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Addition {
+	int a,b,c;
+ public void add(int b,int c) {
+	 this.b=b;
+	 this.c=c;
+	 a=b+c;
+	 System.out.println("Addition of two no. are "+a);
+ }
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/pack2/sample.java
@@ -0,0 +1,11 @@
+package pack2;
+import pack1.Addition;
+public class sample {
+
+	public static void main(String[] args) {
+		Addition a=new Addition();
+		a.add(2,3);
+
+	}
+
+}
